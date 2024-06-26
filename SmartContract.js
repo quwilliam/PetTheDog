8ec3f42b55cdb25a43cd13286dcee718c16b5cbc
quwilliam <159805645+quwilliam@users.noreply.github.com>
2024-06-26 20:14:33 +0200

    Update SmartContract.js

--- SmartContract.js
+++ SmartContract.js
@@ -1,4 +1,4 @@
-let valuetemp = 121;
+let valuetemp = 122;
 console.log( "INIT JS SCRIPT",valuetemp );
 
 // import * as ethers from "https://raw.githubusercontent.com/quwilliam/PetTheDog/main/node_modules/ethers/dist/ethers.min.js";
