014cc38a6188da14a77e19672eb70e612d370854
quwilliam <159805645+quwilliam@users.noreply.github.com>
2024-06-26 20:10:53 +0200

    Update SmartContract.js

--- SmartContract.js
+++ SmartContract.js
@@ -1,10 +1,10 @@
-let valuetemp = 120;
+let valuetemp = 121;
 console.log( "INIT JS SCRIPT",valuetemp );
 
 // import * as ethers from "https://raw.githubusercontent.com/quwilliam/PetTheDog/main/node_modules/ethers/dist/ethers.min.js";
 import * as SmartContractABI from "./SmartContractABICode.js";
-
-import {ethers} from 'https://cdnjs.cloudflare.com/ajax/libs/ethers/6.13.1/ethers.min.js';
+console.log(" TEST ETHER ",ethers);
+// import {ethers} from 'https://cdnjs.cloudflare.com/ajax/libs/ethers/6.13.1/ethers.min.js';
 /*
 var script = document.createElement('script');
 script.src='https://cdnjs.cloudflare.com/ajax/libs/ethers/6.13.1/ethers.min.js';
